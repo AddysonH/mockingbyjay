0a473e17b1177ca17a9462d1486ce99690ce7c84
AddysonH <iamaddyson@gmail.com>
2021-09-06 17:53:31 -0600

    getting event listener

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="wheel">
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
-    <div class="card">{{carousel}}</div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
 
 </div>
 </template>
@@ -30,29 +30,38 @@ export default ({
        
    
    },
-   mounted(){
+   mounted(){  
        let theta = Math.PI / 4.0;
        let new_theta = 0.0;
        let new_x = 0.0;
        let new_y = 0.0;
        let wheel_radius = 200.00;
        const cards = document.querySelectorAll('.card');
+       const wheel = document.querySelector('.wheel');
        const center = {
         x: parseFloat(getComputedStyle(cards[0]).left),
         y: parseFloat(getComputedStyle(cards[0]).top)
     }
 
-    cards.forEach((card, index) => {
-        new_theta = theta * index;
-        new_x = (Math.cos(new_theta) * wheel_radius);
-        new_y = (-1.0 * Math.sin(new_theta) * wheel_radius);
+       cards.forEach((card, index) => {
+          new_theta = theta * index;
+          new_x = (Math.cos(new_theta) * wheel_radius);
+          new_y = (-1.0 * Math.sin(new_theta) * wheel_radius);
 
-        card.style.left = `${center.x + new_x}px`;
-        card.style.top = `${center.y + new_y}px`;
-    });
+          card.style.left = `${center.x + new_x}px`;
+          card.style.top = `${center.y + new_y}px`;
+       });
+
+       
+
+       document.addEventListener('wheel', event => {
+           let scroll_speed = event.wheelDeltaY;
+            wheel.style.transform = `rotate(${theta + scroll_speed})`       })
+     
+
+   }
 
     
-   }
    
 })
 </script>
@@ -62,7 +71,7 @@ export default ({
     width: 300px;
     height: 200px;
     background-color: blue;
-
+ 
 /* This is based off the top left corner of the rectangle. It is placed in the center of the page. */
     position: absolute;
     left: 50%;
